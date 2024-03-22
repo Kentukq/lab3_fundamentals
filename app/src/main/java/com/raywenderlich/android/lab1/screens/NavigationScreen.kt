861b455542d23f8b79a4c3743e0a22fe9f3adf00
Kentukq <kentuukq@gmail.com>
2024-03-22 15:59:16 +0300

    Отображение кнопок под row, column, box. Реализация кнопки box

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -35,6 +35,9 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_progress), Screen.ProgressIndicator)
             NavigationButton(stringResource(id = R.string.button_alertDialog), Screen.AlertDialog)
             NavigationButton(stringResource(id = R.string.porno), Screen.Row)
+            NavigationButton(stringResource(id = R.string.Column), Screen.Column)
+            NavigationButton(stringResource(id = R.string.Box), Screen.Box)
+
         }
     }
 }
